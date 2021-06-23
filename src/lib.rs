0fb718b0ab3664e0e9f955355afd3c52b0a588a6
Alex <gecktor@gmail.com>
2021-06-22 23:06:42 -0700

    Added methods to shift rows left and right

--- src/lib.rs
+++ src/lib.rs
@@ -89,18 +89,20 @@ mod naive_impl {
         return v;
     }
 
-    fn compactify(v: &Vec<i32>) -> Vec<i32> {
-        let mut vec = Vec::new();
-        for i in v {
-            if *i != 0 {
-                vec.push(*i);
-            }
-        }
-        vec.resize(v.len(), 0);
+    fn shift_row_left(v: &Vec<i32>) -> Vec<i32> {
+        let mut vec = compactify_row(v);
+        return combine_paired_cells_in_row(&mut vec);
+    }
+
+    fn shift_row_right(v: &Vec<i32>) -> Vec<i32> {
+        let mut vv = v.clone();
+        vv.reverse();
+        let mut vec = shift_row_left(&vv);
+        vec.reverse();
         return vec;
     }
 
-    fn combine_paired_cells(v: &mut Vec<i32>) -> Vec<i32> {
+    fn combine_paired_cells_in_row(v: &mut Vec<i32>) -> Vec<i32> {
         let mut i = 0;
         while i < v.len() - 1 && v[i] != 0 {
             if v[i] == v[i + 1] {
@@ -111,32 +113,83 @@ mod naive_impl {
                 i += 1
             }
         }
-        return compactify(v);
+        return compactify_row(v);
+    }
+
+    fn compactify_row(v: &Vec<i32>) -> Vec<i32> {
+        let mut vec = Vec::new();
+        for i in v {
+            if *i != 0 {
+                vec.push(*i);
+            }
+        }
+        vec.resize(v.len(), 0);
+        return vec;
+    }
+
+    #[test]
+    fn test_shift_row_left() {
+        let v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
+        assert_eq!(shift_row_left(&v1), vec![4, 4, 0, 0, 0, 0, 0, 0]);
+
+        let v2 = vec![2, 4, 8, 2, 4, 8, 2, 4];
+        assert_eq!(shift_row_left(&v2), vec![2, 4, 8, 2, 4, 8, 2, 4]);
+
+        let v3 = vec![2, 2, 2, 2, 8, 4, 4, 2];
+        assert_eq!(shift_row_left(&v3), vec![4, 4, 8, 8, 2, 0, 0, 0]);
+
+        let v4 = vec![0, 0, 0, 2, 2, 2, 2, 4];
+        assert_eq!(shift_row_left(&v4), vec![4, 4, 4, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_shift_row_right() {
+        let v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
+        assert_eq!(shift_row_right(&v1), vec![0, 0, 0, 0, 0, 0, 4, 4]);
+
+        let v2 = vec![2, 4, 8, 2, 4, 8, 2, 4];
+        assert_eq!(shift_row_right(&v2), vec![2, 4, 8, 2, 4, 8, 2, 4]);
+
+        let v3 = vec![2, 2, 2, 2, 8, 4, 4, 2];
+        assert_eq!(shift_row_right(&v3), vec![0, 0, 0, 4, 4, 8, 8, 2]);
+
+        let v4 = vec![0, 2, 0, 2, 2, 2, 2, 4];
+        assert_eq!(shift_row_right(&v4), vec![0, 0, 0, 0, 2, 4, 4, 4]);
     }
 
     #[test]
     fn test_compactify() {
         let v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
-        assert_eq!(compactify(&v1), vec![2, 2, 4, 0, 0, 0, 0, 0]);
+        assert_eq!(compactify_row(&v1), vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
         let v2 = vec![0, 0, 2, 0, 2, 0, 4, 0];
-        assert_eq!(compactify(&v2), vec![2, 2, 4, 0, 0, 0, 0, 0]);
+        assert_eq!(compactify_row(&v2), vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
         let v3 = vec![2, 2, 4, 0, 0, 0, 0, 0];
-        assert_eq!(compactify(&v3), vec![2, 2, 4, 0, 0, 0, 0, 0]);
+        assert_eq!(compactify_row(&v3), vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
         let v4 = vec![2, 2, 4];
-        assert_eq!(compactify(&v4), vec![2, 2, 4]);
+        assert_eq!(compactify_row(&v4), vec![2, 2, 4]);
     }
+
     #[test]
     fn test_combine_paired_cells() {
         let mut v1 = vec![2, 2, 2, 2, 4, 8, 0, 0];
-        assert_eq!(combine_paired_cells(&mut v1), vec![4, 4, 4, 8, 0, 0, 0, 0]);
+        assert_eq!(
+            combine_paired_cells_in_row(&mut v1),
+            vec![4, 4, 4, 8, 0, 0, 0, 0]
+        );
 
         let mut v2 = vec![2, 4, 8, 2, 4, 8, 2, 4];
-        assert_eq!(combine_paired_cells(&mut v2), vec![2, 4, 8, 2, 4, 8, 2, 4]);
+        assert_eq!(
+            combine_paired_cells_in_row(&mut v2),
+            vec![2, 4, 8, 2, 4, 8, 2, 4]
+        );
 
         let mut v3 = vec![2, 4, 4, 4, 0, 0, 0, 0];
-        assert_eq!(combine_paired_cells(&mut v3), vec![2, 8, 4, 0, 0, 0, 0, 0]);
+        assert_eq!(
+            combine_paired_cells_in_row(&mut v3),
+            vec![2, 8, 4, 0, 0, 0, 0, 0]
+        );
     }
 }
