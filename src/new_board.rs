a0ba07ab5c99d7e26ed0fb3e237fdcf3496945d2
Alex <gecktor@gmail.com>
2021-07-05 23:56:03 -0700

    Added compactification functions for rows.

--- src/new_board.rs
+++ src/new_board.rs
@@ -6,7 +6,7 @@ struct Board<R: RandomNumberGenerator> {
     generator: TileGenerator<R>,
 }
 
-fn compactify_row(v: &mut [i32]) {
+fn compactify_row_left(v: &mut [i32]) {
     let first_empty_opt = find_first_empty(v);
     let mut first_empty = match first_empty_opt {
         Some(index) => index,
@@ -26,6 +26,26 @@ fn compactify_row(v: &mut [i32]) {
     }
 }
 
+fn compactify_row_right(v: &mut [i32]) {
+    let first_empty_opt = find_last_empty(v);
+    let mut first_empty = match first_empty_opt {
+        Some(index) => index,
+        None => return,
+    };
+    let mut current: i32 = first_empty as i32 - 1;
+    while current > -1 {
+        while current > -1 && v[current as usize] == 0 {
+            current -= 1;
+        }
+        if current == -1 {
+            return;
+        }
+        v.swap(first_empty, current as usize);
+        first_empty -= 1;
+        current -= 1;
+    }
+}
+
 fn find_first_empty(v: &[i32]) -> Option<usize> {
     return v.iter().position(|&x| x == 0);
 }
@@ -37,24 +57,47 @@ fn find_last_empty(v: &[i32]) -> Option<usize> {
 impl<R: RandomNumberGenerator> Board<R> {}
 
 #[test]
-fn test_compactify() {
+fn test_compactify_row_left() {
     let mut v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
-    compactify_row(&mut v1);
+    compactify_row_left(&mut v1);
     assert_eq!(v1, vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
     let mut v2 = vec![0, 0, 2, 0, 2, 0, 4, 0];
-    compactify_row(&mut v2);
+    compactify_row_left(&mut v2);
     assert_eq!(v2, vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
     let mut v3 = vec![2, 2, 4, 0, 0, 0, 0, 0];
-    compactify_row(&mut v3);
+    compactify_row_left(&mut v3);
     assert_eq!(v3, vec![2, 2, 4, 0, 0, 0, 0, 0]);
 
     let mut v4 = vec![2, 2, 4];
-    compactify_row(&mut v4);
+    compactify_row_left(&mut v4);
     assert_eq!(v4, vec![2, 2, 4]);
 
     let mut v5 = vec![0, 2, 0, 0, 0, 2, 4, 4, 0, 2, 0, 2, 2];
-    compactify_row(&mut v5);
+    compactify_row_left(&mut v5);
     assert_eq!(v5, vec![2, 2, 4, 4, 2, 2, 2, 0, 0, 0, 0, 0, 0]);
+}
+
+#[test]
+fn test_compactify_row_right() {
+    let mut v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
+    compactify_row_right(&mut v1);
+    assert_eq!(v1, vec![0, 0, 0, 0, 0, 2, 2, 4]);
+
+    let mut v2 = vec![0, 0, 2, 0, 2, 0, 4, 0];
+    compactify_row_right(&mut v2);
+    assert_eq!(v2, vec![0, 0, 0, 0, 0, 2, 2, 4]);
+
+    let mut v3 = vec![2, 2, 4, 0, 0, 0, 0, 0];
+    compactify_row_right(&mut v3);
+    assert_eq!(v3, vec![0, 0, 0, 0, 0, 2, 2, 4]);
+
+    let mut v4 = vec![2, 2, 4];
+    compactify_row_right(&mut v4);
+    assert_eq!(v4, vec![2, 2, 4]);
+
+    let mut v5 = vec![0, 2, 0, 0, 0, 2, 4, 4, 0, 2, 0, 2, 2];
+    compactify_row_right(&mut v5);
+    assert_eq!(v5, vec![0, 0, 0, 0, 0, 0, 2, 2, 4, 4, 2, 2, 2]);
 }
