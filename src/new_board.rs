f4a74a8ae8ee2a60b1f6038fb79f419943f59b33
Alex <gecktor@gmail.com>
2021-07-07 23:13:10 -0700

    Added in-place transpose for square matrix.

--- src/new_board.rs
+++ src/new_board.rs
@@ -6,6 +6,18 @@ struct Board<R: RandomNumberGenerator> {
     generator: TileGenerator<R>,
 }
 
+impl<R: RandomNumberGenerator> Board<R> {}
+
+fn transpose(cells: &mut Vec<Vec<i32>>) {
+    for i in 0..cells.len() {
+        for j in 0..i {
+            let temp = cells[i][j];
+            cells[i][j] = cells[j][i];
+            cells[j][i] = temp;
+        }
+    }
+}
+
 fn compactify_row_left(v: &mut [i32]) {
     let first_empty_opt = find_first_empty(v);
     let mut first_empty = match first_empty_opt {
@@ -54,8 +66,6 @@ fn find_last_empty(v: &[i32]) -> Option<usize> {
     return v.iter().rposition(|&x| x == 0);
 }
 
-impl<R: RandomNumberGenerator> Board<R> {}
-
 #[test]
 fn test_compactify_row_left() {
     let mut v1 = vec![2, 0, 0, 0, 2, 0, 4, 0];
@@ -100,4 +110,44 @@ fn test_compactify_row_right() {
     let mut v5 = vec![0, 2, 0, 0, 0, 2, 4, 4, 0, 2, 0, 2, 2];
     compactify_row_right(&mut v5);
     assert_eq!(v5, vec![0, 0, 0, 0, 0, 0, 2, 2, 4, 4, 2, 2, 2]);
+}
+
+#[test]
+fn test_transpose_square_matrix() {
+    let mut v = vec![
+        vec![1, 2, 3, 4],
+        vec![5, 6, 7, 8],
+        vec![9, 10, 11, 12],
+        vec![13, 14, 15, 16],
+    ];
+    transpose(&mut v);
+    assert_eq!(
+        v,
+        vec![
+            vec![1, 5, 9, 13],
+            vec![2, 6, 10, 14],
+            vec![3, 7, 11, 15],
+            vec![4, 8, 12, 16]
+        ]
+    );
+}
+
+#[test]
+fn test_transpose_identity_matrix() {
+    let mut v = vec![
+        vec![1, 0, 0, 0],
+        vec![0, 1, 0, 0],
+        vec![0, 0, 1, 0],
+        vec![0, 0, 0, 1],
+    ];
+    transpose(&mut v);
+    assert_eq!(
+        v,
+        vec![
+            vec![1, 0, 0, 0],
+            vec![0, 1, 0, 0],
+            vec![0, 0, 1, 0],
+            vec![0, 0, 0, 1]
+        ]
+    );
 }
